1801e53a0ebc36fffe7c9c9add82d0a6f7c217bc
Can <93058208+xxaalpha@users.noreply.github.com>
2021-12-26 23:30:43 +0300

    Add files via upload

---
+++ assets/js/languages.js
@@ -0,0 +1,269 @@
+$(document).ready(function() {
+        
+    var arrLang={
+        
+        'tr':{
+    
+            '0':'Satın Almak',
+            '1':'Satmak',
+            '2':'Kripto Para Birimleri',
+            '3':'Destek',
+            '4':'Blog',
+            '5':'TR',
+             
+            '7':'Ne?',
+  
+            '8':'Şimdi Başla!',
+            '9':'Kolay, Hızlı ve Güvenli!',
+            '10':'Kripto Para Al ve Sat',
+            '11':'Caliscoin, kripto para biriminizi satın almak, satmak için en kolay yer',
+            '12':"Var",
+            '13':'Alırsın',
+            '17':'Arama Yap ?',
+            '15':'Girit Mediterranea denizinde bir yunan adasıdır ',
+            '16':"Nasıl?",
+            '17':"Bitcoin Sat",
+            '18':"10.000'den fazla incelemeye göre!",
+            '19':'Arama Yap ?',
+            '20':'İsim',
+            '21':'Fiyat',
+            '22':'Değişiklik',
+            '23':'24s',
+            '24':'Piyasa değeri',
+            '25':'Çizelge',
+            '26':'24s',
+            '27':'Satmak',
+            '28':'Satın almak',
+            '29':'Sat',
+            '30':'Al',
+            '31':'Kripto para birimi nasıl alınır ve satılır',
+            '32':'Gerçekten çok kolay!',
+            '33':'Düşündüğünüzden daha kolay. 3 basit kolay adımı izleyin',
+            '34':'Almak/Satmak istediğiniz kriptoyu seçin',
+            '35':'Hesap doğrulamasını geç',
+            '36':'Kripto para birimi satın al ve sat',
+            '37':'Dakikalar içinde paranın geleceğine erişin',
+            '38':'En güvenilir kripto para birimi platformu',
+            '39':'İşte Caliscoinˋi seçmeniz için birkaç neden',
+            '40':'Şimdi Başlayın!',
+            '41':'Kripto satın almanın, satmanın hızlı ve basit yolu',
+            '42':'Profesyonel, Güvenli ve Güvenli',
+            '43':'Çoklu Para Birimi Desteği',
+            '44':'Çalışanlarımız 7/24 sizin için hazır',
+            '45':'Ödeme Yöntemlerimiz:',
+            '46':'Referanslar',
+            '47':'Binlerce Mutlu Müşteri Tüm dünyada',
+            '48':'100ˋden fazla ülkeden müşteriler tarafından güvenilen',
+            '49':'Kripto para alışverişi',
+            '50':'Desteklenen ülkeler',
+            '51':'Hizmet verilen müşteriler',
+            '52':'Şirket',
+            '53':'Destek',
+            '54':'Politika',
+            '55':'Hizmetler',
+            '56':'Topluluk',
+            '57':'Kişiler',
+            '58':'Şimdi Katıl',
+  
+  
+        
+            '59':'Hakkımızda',
+  
+            '60':'Bağlı Kuruluş',
+  
+            '61':'Blog',
+  
+            '62':'SSS',
+  
+            '63':'İletişim',
+  
+            '64':'Bilgi Tabanı',
+  
+            '65':'Kullanım Şartları',
+  
+            '66':'Gizlilik Politikası',
+  
+            '67':'Geri ödeme politikası',
+  
+            '68':'Satın Almak',
+  
+            '69':'Satmak',
+  
+  
+            '70':'Yakınınızdan Satın Alın',
+  
+            '71':'Yönlendirme Programı',
+  
+            '72':'Ortaklık Programı',
+  
+            '73':'Caliscoin Blog',
+  
+            '74':'[e-posta korumalı]',
+  
+  
+  
+  
+  
+  
+            '79':'Dil Seçin',
+  
+  
+  
+            '80':'testtt'
+            
+             
+            
+            
+            
+  
+            
+    
+    
+    
+    
+        },
+    
+    
+        'en':{
+            '0':'Purchasing',
+            '1':'To sell',
+            '2':'Cryptocurrencies',
+            '3':'Support',
+            '4':'Blog',
+            '5':'TR',
+           
+           
+            '7':'What?',
+            '8':'Start Now !',
+            '9':'Easy, Fast and Secure!',
+            '10':'Buy & Sell Cryptocurrency',
+            '11':'Caliscoin is the easiest place to buy, sell your cryptocurrency',
+            '12':'You have',
+            '13':'You Get',
+            '14':'Search ?',
+            '15':'Girit Mediterranea denizinde bir yunan adasıdır ',
+            '16':"Nasıl?",
+            '17':"Sell ​​Bitcoin",
+            '18':'Based on 10.000+reviews!',
+            '19':'Search ?',
+  
+  
+            '20':'Name',
+            '21':'Price',
+            '22':'Change',
+            '23':'24h',
+            '24':'Market value',
+            '25':'chart',
+            '26':'24h',
+            '27':'To sell',
+            '28':'Purchasing',
+            '29':'Sell',
+            '30':'Get',
+            '31':'How to buy and sell cryptocurrency',
+            '32':'Itˋs really easy!',
+            '33':'Itˋs easier than you think. Follow 3 simple easy steps',
+            '34':'Select the crypto you want to Buy/Sell',
+            '35':'Pass account verification',
+            '36':'Buy and sell cryptocurrency',
+            '37':'Access the future of money in minutes',
+            '38':'The most trusted cryptocurrency platform',
+            '39':'Here are a few reasons to choose Caliscoin',
+            '40':'Get Started Now!',
+            '41':'Fast and simple way to buy, sell crypto',
+            '42':'Professional, Safe and Secure',
+            '43':'Multi-Currency Support',
+            '44':'Our employees are ready for you 24/7',
+            '45':'Our Payment Methods:',
+            '46':'References',
+            '47':'Thousands of Happy Customers All Over the World',
+            '48':'Trusted by customers from over 100 countries',
+            '49':'Cryptocurrency exchange',
+            '50':'Supported countries',
+            '51':'Customers served',
+            '52':'Company',
+            '53':'Support',
+            '54':'Policy',
+            '55':'Services',
+            '56':'Community',
+            '57':'Persons',
+            '58':'Join Now',
+  
+             
+  
+          
+            '59':'About Us',
+  
+            '60':'Affiliate',
+  
+            '61':'Blog',
+  
+            '62':'FAQ',
+  
+            '63':'Contact',
+  
+            '64':'Knowledge Base',
+  
+            '65':'Terms of use',
+  
+            '66':'Privacy Policy',
+  
+            '67':'Refund Policy',
+  
+            '68':'Buy',
+  
+            '69':'Sell',
+  
+  
+            '70':'Buy Near You',
+  
+            '71':'Referral Program',
+  
+            '72':'Affiliate Program',
+  
+            '73':'Caliscoin Blog',
+  
+            '74':'[email protected]',
+  
+  
+  
+  
+  
+  
+  
+            '79':'Select Language',
+  
+            '80':'testtt'
+          
+            
+  
+    
+    
+        },
+        
+        
+    };
+    
+    
+    
+    $('.dil').click(function() {
+    localStorage.setItem('dil', JSON.stringify($(this).attr('id'))); 
+    location.reload();
+    });
+    
+    var lang =JSON.parse(localStorage.getItem('dil'));
+    
+    if(lang=="en"){
+        $("#drop_yazı").html("EN");
+    }
+    else{
+        $("#drop_yazı").html("TR");
+    
+    }
+    
+    $('a,h5,h4,p,h1,h2,span,li,button,h3,label').each(function(index,element) {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    
+    });
+    
+    });
+    
